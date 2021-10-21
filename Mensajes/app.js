b3ac4d3868a07588ab405a0935e526fb42b0a21e
MajoS7 <85207019+MajoS7@users.noreply.github.com>
2021-10-21 08:32:38 -0500

    Se actualizo el ejercicio Mensajes

--- Mensajes/app.js
+++ Mensajes/app.js
@@ -11,39 +11,20 @@ sendButtom.addEventListener('click', sendMessage)
 
 function captureLetter() {
     initialetter.textContent = name1.value.charAt(0).toUpperCase();
-    
 }
 
-message.addEventListener('keydown', captureMessage)
+message.addEventListener('keyup', captureMessage)
 
 function captureMessage(){
-    const numberLetters = message.value.length+1;
-    let max_quantity = 201;
+    const numberLetters = message.value.length;
+    let max_quantity = 200;
     let subtract = max_quantity-numberLetters;
 
     number.textContent=subtract;
 
-    if (subtract<=50){
-        number.style.color="red";
-        if (subtract<=0){
-            message.disabled = true;
-        }
-        else{
-            message.disabled = false;
-        }
-    }
-    else{
-        number.style.color="black";
-    }
-
-
-    
+    if (subtract<=50){number.style.color="red";}
+    else{number.style.color="black";}
 }
 
 
-function sendMessage() {
-
-    diplayMessage.textContent=message.value;
-    
-    
-}
+function sendMessage() {diplayMessage.textContent=message.value;}
