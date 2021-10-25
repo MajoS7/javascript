aa8bfd257483ecf0423161b9ef1b8b52497764bd
MajoS7 <85207019+MajoS7@users.noreply.github.com>
2021-10-25 01:04:25 -0500

    Creacion de la tarjeta con javascript

--- card/app.js
+++ card/app.js
@@ -1,6 +1,7 @@
 const user= document.querySelector("#user");
 const message =document.querySelector("#message");
 const generate = document.querySelector("#generate");
+const card =document.querySelector("#card");
 
 user.addEventListener('keyup', captureCharacter);
 
@@ -28,6 +29,41 @@ function captureCharacter() {
         generate.disabled =false;
     }
     
+}
+
+generate.addEventListener('click',createCard);
+
+function createCard() {
+    let cuadro=document.createElement('div');
+    cuadro.style.width='80%';
+    cuadro.style.height='90%';
+    cuadro.style.borderRadius='30px';
+    cuadro.style.boxShadow='0px 0px 20px rgb(230,18,18)'
+    cuadro.style.display='flex';
+    cuadro.style.flexDirection='column';
+    
+    cuadro.style.alignItems='center';
+    
+    card.appendChild(cuadro);
+
+    let textuser= document.createElement('p');
+    
+    textuser.style.width='55%';
+    textuser.style.height='55%';
+    textuser.style.marginTop='10%';
+    textuser.style.boxShadow='rgb(0 0 0 / 60%) 0px -7px 7px';
+    textuser.style.borderRadius='137px';
+    textuser.textContent=user.value;
+    textuser.style.display='flex';
+    textuser.style.justifyContent='center';
+    textuser.style.alignItems='center';
+    cuadro.appendChild(textuser);
+
+
+    let welcome =document.createElement('h2');
+    welcome.textContent="Bienvenido al sistema";
+    cuadro.appendChild(welcome);
+
 }
 
 
